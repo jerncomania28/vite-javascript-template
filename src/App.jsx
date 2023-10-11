5bebfc35cd1afff26a632e202c1cba2d8e9d015f
jerncomania28 <okonnduesojeremiah@gmail.com>
2023-10-11 11:32:51 +0100

    bolier plate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from '@mui/material';
+
+//components
+import Sample from './components/Sample.jsx';
+
+function App() {
+  return (
+    <Container>
+      <Typography>Vite Javascript Template</Typography>
+      <Sample />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Sample.jsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+
+const Sample = () => {
+  return <Typography variant="h1">Sample Component</Typography>;
+};
+
+export default Sample;
